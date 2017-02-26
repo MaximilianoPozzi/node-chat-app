73d49b6cbee13b5b4ba820bb505696da6756df3b
Maximiliano Pozzi <maximilianopozzidp@gmail.com>
2017-02-26 03:53:23 -0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,24 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // socket.emit('newEmail', {
-    //     from: 'mike@example.com',
-    //     text: 'Hey. What is going on.',
-    //     createdAt: 123
-    // });
-
-    socket.emit('newMessage', {
-        from: 'Jen',
-        text: 'This is the second message I am sending',
-        createdAt: 1234
-    });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
-
     socket.on('createMessage', (createMessage) => {
         console.log('createMessage', createMessage);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
