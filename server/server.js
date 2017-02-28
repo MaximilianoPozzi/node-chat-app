a48b1bfbb101bc07333bae328fd4fbef61b9f2ff
Maximiliano Pozzi <maximilianopozzidp@gmail.com>
2017-02-27 21:29:51 -0300

    Correct minor errors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to teh chat app'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
@@ -23,9 +23,6 @@ io.on('connection', (socket) => {
         console.log('createMessage', createMessage);
         io.emit('newMessage', generateMessage(createMessage.from, createMessage.text));
         callback('This is from the server.');
-
-        io.emit('newMessage', generateMessage(createMessage.from, createMessage.text));
-
     });
 
     socket.on('disconnect', () => {
